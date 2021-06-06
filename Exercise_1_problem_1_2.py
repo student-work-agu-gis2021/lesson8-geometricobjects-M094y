acb14d0c26481fab8a217c4f5fe439a7ecf7f302
M094y <>
2021-06-06 16:55:11 +0000

    I created a function "create_point_geom()".

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -11,7 +11,9 @@
 
 from shapely.geometry import Point, LineString, Polygon
 #YOUR CODE HERE 1 to define create_point_geom()
-
+def create_point_geom(x_coord, y_coord):
+  point=Point(x_coord,y_coord)
+  return(point)
 # Test your function by running these code cells:
 
 # CODE FOR TESTING YOUR SOLUTION
@@ -27,12 +29,16 @@ print(point1.geom_type)
 # 
 
 # YOUR CODE HERE 2 to define create_line_geom()
-
+def create_line_geom(points):
+  assert type(points)=="Input should be a list!"
+  assert len(points)>=2,"LineString object requires at least two Points!"
+  line = LineString([points[0],points[1]])
+  return(line)
 # Demonstrate the usage of your function; For example, create a line object with two points: `Point(45.2, 22.34)` & `Point(100.22, -3.20)` and store the result in a variable called `line1`:
 
 line1 = None
 # YOUR CODE HERE 3 to define two points and store the result in line1
-
+line1 = LineString([Point(45.2, 22.34),Point(100.22, -3.20)])
 
 # CODE FOR TESTING YOUR SOLUTION
 print(line1)
@@ -63,11 +69,17 @@ except Exception as e:
 
 
 # YOUR CODE HERE 4 to define create_poly_geom()
-
+def create_poly_geom(coords):
+  assert type(coords) is list, "Input should be a list!"
+  assert len(coords)>=3,"Polygon object requires at least three Points!"
+  for i in coords:
+    assert type(i) is tuple, "All list values should be coordinate tuples!"
+  poly = Polygon(coords)
+  return(poly)
 # Demonstrate the usage of the function. For example, create a Polygon with three points: `(45.2, 22.34)`, `(100.22, -3.20)` & `(70.0, 10.20)`.
 
 # YOUR CODE HERE 5 to define poly1 with three points
-poly1 = 
+poly1 = create_poly_geom([(45.2, 22.34),(100.22, -3.20),(70.0, 10.20)])
 
 # CODE FOR TESTING YOUR SOLUTION
 print(poly1)
@@ -98,12 +110,14 @@ except Exception as e:
 # 
 
 #  YOUR CODE HERE 6 to define get_centroid()
-
-# Test and demonstrate the usage of the function. You can, for example, create shapely objects using the functions you created in problem 1 and print out information about their centroids:
+def get_centroid(geom):
+  assert type(geom) == Point or type(geom) == LineString or type(geom) == Polygon, "Input should be a Shapely geometry!"
+  return geom.centroid
+  # Test and demonstrate the usage of the function. You can, for example, create shapely objects using the functions you created in problem 1 and print out information about their centroids:
 # 
 
 #  YOUR CODE HERE 7 to define some objects
-
+poly1 = create_poly_geom([(45.2, 22.34),(100.22, -3.20),(70.0, 10.20)])
 
 # CODE FOR TESTING YOUR SOLUTION
 centroid = get_centroid(poly1)
